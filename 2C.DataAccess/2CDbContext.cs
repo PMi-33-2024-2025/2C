467baba5f4fb06f58f32ac21515b19c6d52657c2
AndrD1406 <andriy.dolishniy2005@gmail.com>
2024-09-30 20:52:07 +0300

    Added DbContext

---
+++ 2C.DataAccess/2CDbContext.cs
@@ -0,0 +1,30 @@
+﻿using _2C.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2C.DataAccess
+{
+    public class _2CDbContext: DbContext
+    {
+        public _2CDbContext(DbContextOptions<_2CDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
