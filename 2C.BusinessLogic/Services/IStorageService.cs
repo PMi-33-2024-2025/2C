cf221bc8b607ea55d9f0643cf33459f0243d2550
danylo kozlovskij <danilokozlovskij@gmail.com>
2024-10-28 21:43:50 +0200

    Added StorageService

---
+++ 2C.BusinessLogic/Services/IStorageService.cs
@@ -0,0 +1,18 @@
+﻿using _2C.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2C.BusinessLogic.Services
+{
+	public interface IStorageService
+	{
+		public Task Create(Storage storage);
+		public Task<IEnumerable<Storage>> GetAll();
+		public Task<Storage> GetById(long id);
+		public Task Update(long id, string address);
+		public Task Delete(long id);
+	}
+}
