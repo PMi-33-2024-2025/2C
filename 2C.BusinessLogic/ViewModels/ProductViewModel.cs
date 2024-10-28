3a003a9a13814a20ea01d65c4ecf7c17e5685a47
Inn21 <innicasandretorz@gmail.com>
2024-10-28 22:13:40 +0200

    Chaged ui. Added CURD buttons. Started work on CRUD impementation

---
+++ 2C.BusinessLogic/ViewModels/ProductViewModel.cs
@@ -0,0 +1,76 @@
+﻿using _2C.BusinessLogic.Services;
+using _2C.DataAccess.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _2C.BusinessLogic.ViewModels
+{
+    public class ProductViewModel : INotifyPropertyChanged
+    {
+        private readonly IProductService _productService;
+
+        public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>();
+
+        private Product _selectedProduct;
+        public Product SelectedProduct
+        {
+            get => _selectedProduct;
+            set
+            {
+                if (_selectedProduct != value)
+                {
+                    _selectedProduct = value;
+                    OnPropertyChanged(nameof(SelectedProduct));
+                }
+            }
+        }
+
+        // Constructor
+        public ProductViewModel(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // Methods to interact with ProductService
+        public async Task LoadProducts()
+        {
+            var products = await _productService.GetAll();
+            Products.Clear();
+            foreach (var product in products)
+                Products.Add(product);
+        }
+
+        public async Task AddProduct(Product product)
+        {
+            await _productService.Create(product);
+            await LoadProducts();
+        }
+
+        public async Task UpdateProduct()
+        {
+            if (_selectedProduct != null)
+            {
+                await _productService.Update(_selectedProduct.Id, _selectedProduct.Name, _selectedProduct.Price, _selectedProduct.Quantity, _selectedProduct.StorageId);
+                await LoadProducts();
+            }
+        }
+
+        public async Task DeleteProduct()
+        {
+            if (_selectedProduct != null)
+            {
+                //await _productService.Delete(_selectedProduct.Id);
+                await LoadProducts();
+            }
+        }
+
+        // INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- 2C/MainWindow.xaml.cs
+++ 2C/MainWindow.xaml.cs
@@ -1,24 +1,61 @@
-﻿using System.Text;
+﻿using _2C.BusinessLogic.Services;
+using _2C.BusinessLogic.ViewModels;
+using _2C.DataAccess;
+using _2C.DataAccess.Models;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _2C
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ProductViewModel _productViewModel;
+
+        // Parameterless constructor required by WPF
         public MainWindow()
         {
             InitializeComponent();
+            _productViewModel = new ProductViewModel(new ProductService(new _2CDbContext())); // Instantiate ProductService or use Dependency Injection
+            DataContext = _productViewModel;
         }
+
+        // Constructor with dependency injection for ProductViewModel
+        public MainWindow(ProductViewModel productViewModel)
+        {
+            InitializeComponent();
+            _productViewModel = productViewModel;
+            DataContext = _productViewModel;
+        }
+
+        private async void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            //// Code to add a new product
+            //var newProduct = new Product { /* Initialize with properties */ };
+            //await _productViewModel.AddProduct(newProduct);
+        }
+
+        private async void EditProduct_Click(object sender, RoutedEventArgs e)
+        {
+            // Code to edit the selected product
+            if (_productViewModel.SelectedProduct != null)
+            {
+                await _productViewModel.UpdateProduct();
+            }
+        }
+
+        private async void DeleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+            // Code to delete the selected product
+            if (_productViewModel.SelectedProduct != null)
+            {
+                await _productViewModel.DeleteProduct();
+            }
+        }
+
+        private async void RefreshProducts_Click(object sender, RoutedEventArgs e)
+        {
+            // Refresh the product list
+            await _productViewModel.LoadProducts();
+        }
+
     }
 }
