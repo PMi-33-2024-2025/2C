7d28108f5f24690ee39b9d359aa799a163e0d0ad
danylo kozlovskij <danilokozlovskij@gmail.com>
2024-10-28 21:20:01 +0200

    modified UserService

--- 2C.BusinessLogic/Services/UserService.cs
+++ 2C.BusinessLogic/Services/UserService.cs
@@ -6,9 +6,11 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace _2C.BusinessLogic.Services
 {
