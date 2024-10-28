d6db338358a448e626b14f4833d32b5631df5155
AndrD1406 <andriy.dolishniy2005@gmail.com>
2024-10-28 20:21:52 +0200

    Product service, added the Create and GetAll methods

--- 2C.BusinessLogic/Services/ProductService.cs
+++ 2C.BusinessLogic/Services/ProductService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _2C.DataAccess;
+using _2C.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,25 @@ using System.Threading.Tasks;
 
 namespace _2C.BusinessLogic.Services
 {
-    public class ProductService
+    public class ProductService: IProductService
     {
+        private readonly _2CDbContext context;
 
+        public ProductService(_2CDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task Create(Product product)
+        {
+
+            await context.AddAsync(product).ConfigureAwait(false);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await context.Products.ToListAsync().ConfigureAwait(false);
+        }
     }
 }
