4fb4b72fc0beea773406caafef2c2980eec2e8d7
danylo kozlovskij <danilokozlovskij@gmail.com>
2024-11-19 00:26:53 +0200

    added bogus

--- 2C.BusinessLogic.Tests/ProductServiceTests.cs
+++ 2C.BusinessLogic.Tests/ProductServiceTests.cs
@@ -1,22 +1,44 @@
 ﻿using _2C.BusinessLogic.Services;
 using _2C.DataAccess;
+using _2C.DataAccess.Models;
+using Bogus;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace _2C.BusinessLogic.Tests
 {
 	[TestFixture]
 	public class ProductServiceTests
 	{
-		private IProductService service;
+		private Mock<ProductService> service;
+		private Faker<Product> faker = new Faker<Product>()
+			.RuleFor(x => x.Id, _ => new Guid())
+			.RuleFor(x => x.Name, _ => )
 		[SetUp]
 		public void SetUp()
 		{
-			service = new ProductService(new Mock<_2CDbContext>().Object);
+			service = new Mock<ProductService>(new Mock<_2CDbContext>().Object);
+		}
+		[Test]
+		public async Task GetById_WhenNonExistentId_ReturnsNull()
+		{
+			// Arrange
+			var products = GenerateProducts();
+			Guid nonExistentId = Guid.NewGuid();
+			// Act
+			service.Setup(m => m.GetById(nonExistentId)).Returns(Task.FromResult<Product>(null));
+			// Assert
+			service.VerifyAll();
+		}
+
+		private List<Application> GenerateApplicationsForSameWorkshop()
+		{
 
 		}
 	}
+}
--- 2C.BusinessLogic/Services/ProductService.cs
+++ 2C.BusinessLogic/Services/ProductService.cs
@@ -9,50 +9,50 @@ using System.Threading.Tasks;
 
 namespace _2C.BusinessLogic.Services
 {
-    public class ProductService: IProductService
-    {
-        private readonly _2CDbContext context;
+	public class ProductService : IProductService
+	{
+		private readonly _2CDbContext context;
 
-        public ProductService(_2CDbContext context)
-        {
-            this.context = context;
-        }
+		public ProductService(_2CDbContext context)
+		{
+			this.context = context;
+		}
 
-        public async Task Create(Product product)
-        {
+		public async Task Create(Product product)
+		{
 
-            await context.AddAsync(product).ConfigureAwait(false);
-            await context.SaveChangesAsync();
-        }
+			await context.AddAsync(product).ConfigureAwait(false);
+			await context.SaveChangesAsync();
+		}
 
-        public async Task<IEnumerable<Product>> GetAll()
-        {
-            return await context.Products.ToListAsync().ConfigureAwait(false);
-        }
+		public async Task<IEnumerable<Product>> GetAll()
+		{
+			return await context.Products.ToListAsync().ConfigureAwait(false);
+		}
 
-        public async Task<Product> GetById(Guid id)
-        {
-            return await context.Products.FirstOrDefaultAsync(x => x.Id == id).ConfigureAwait(false);
-        }
+		public async Task<Product> GetById(Guid id)
+		{
+			return await context.Products.FirstOrDefaultAsync(x => x.Id == id).ConfigureAwait(false);
+		}
 
-        public async Task Update(Guid id, string name, double price, int quantity, long storageId)
-        {
-            var productToUpdate = new Product { Id = id, Name = name, Price = price , Quantity = quantity};
-            var product = await context.Products.FirstOrDefaultAsync(x => x.Id == id).ConfigureAwait(false);
+		public async Task Update(Guid id, string name, double price, int quantity, long storageId)
+		{
+			var productToUpdate = new Product { Id = id, Name = name, Price = price, Quantity = quantity };
+			var product = await context.Products.FirstOrDefaultAsync(x => x.Id == id).ConfigureAwait(false);
 
-            product = productToUpdate;
-            product.StorageId = storageId;
-            await context.SaveChangesAsync().ConfigureAwait(false);
-        }
+			product = productToUpdate;
+			product.StorageId = storageId;
+			await context.SaveChangesAsync().ConfigureAwait(false);
+		}
 
-        public async Task Delete(Guid id)
-        {
-            var productToRemove = await context.Products.FirstOrDefaultAsync(x => x.Id == id).ConfigureAwait(false);
-            if(productToRemove != null)
-            {
-                context.Products.Remove(productToRemove);
-            }
-            await context.SaveChangesAsync().ConfigureAwait(false);
-        }
-    }
+		public async Task Delete(Guid id)
+		{
+			var productToRemove = await context.Products.FirstOrDefaultAsync(x => x.Id == id).ConfigureAwait(false);
+			if (productToRemove != null)
+			{
+				context.Products.Remove(productToRemove);
+			}
+			await context.SaveChangesAsync().ConfigureAwait(false);
+		}
+	}
 }
